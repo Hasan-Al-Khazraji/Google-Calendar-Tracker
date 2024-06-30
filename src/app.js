195834706263d2e711bfb60c19d7bd771f054e2c
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-06-29 22:19:07 -0400

    Add frontend changes to time tracker

--- src/app.js
+++ src/app.js
@@ -4,7 +4,43 @@ const init = function(){
 
 const send = function(ev){
     ev.preventDefault();
-    document.getElementById('theForm').submit();
+
+    const color = document.getElementById('color').value;
+    const title = document.getElementById('title');
+    const timer = document.getElementById('timer');
+
+    const button = document.getElementById('btnSubmit');
+    const colors = document.getElementsByName('color');
+
+    // Start timer
+    if (button.children[0].innerHTML != 'End Timer'){
+        
+        // Front end shenanigans
+        button.children[0].innerHTML = "End Timer";
+        button.style.backgroundColor = 'gray'
+
+        title.disabled = true;
+
+        colors.forEach((element) => {
+            element.disabled = true;
+        })
+
+        // Timer
+        timer.innerHTML = "00:00:01"
+
+
+    }
+    else {
+        button.children[0].innerHTML = "Start Timer";
+        button.style.backgroundColor = ''
+
+        title.disabled = false;
+
+        colors.forEach((element) => {
+            element.disabled = false;
+        })
+    }
+
 }
 
 // Initalizes after DOM content is loaded
